e02c7577814210e7fff2902dca96c40fcb51a52b
Davidniw <davidniw@hotmail.com>
2016-10-21 14:56:12 +1100

    Add files via upload

---
+++ BuildFarm.ps1
@@ -0,0 +1,43 @@
+configuration BuildFarm
+{ 
+    
+    Import-DscResource –ModuleName 'PSDesiredStateConfiguration'
+
+    $storageCredential = Get-AutomationPSCredential -Name 'domainCreds'
+
+    Node TeamCity
+    {
+        WindowsFeature IIS
+        {
+            Ensure               = 'Absent'
+            Name                 = 'Web-Server'
+
+        }
+    }
+
+    Node SonarQube
+    {
+    	File SQLBinaryDownload
+    	{
+    		DestinationPath = "C:\SQLInstall"
+    		Credential = $storageCredential
+    		Ensure = "Present"
+    		SourcePath = "\\prodrockcoresoftware.file.core.windows.net\software\SQLJDBC"
+    		Type = "Directory"
+    		Recurse = $true
+     	}
+
+
+        LocalConfigurationManager 
+        { 
+            CertificateId = $node.Thumbprint 
+        }
+
+        WindowsFeature IIS
+        {
+            Ensure               = 'Absent'
+            Name                 = 'Web-Server'
+
+        }
+    }
+}
