ada7ff2fa2f412fb3cabf21ddff8e5449d6d83c3
Davidniw <davidniw@hotmail.com>
2016-11-11 14:23:12 +1100

    Update BuildFarm.ps1

--- BuildFarm.ps1
+++ BuildFarm.ps1
@@ -27,15 +27,6 @@ configuration BuildFarm
     		Recurse = $true
      	}
         
-        Package PackageExample
-        {
-            Ensure      = "Present"
-            Path        = "c:\sonarqube-6.0\bin\windows-x86-64\InstallNTService.bat"
-            Name        = "sonarqube"
-            ProductId   = "ACDDCDAF-80C6-41E6-A1B9-8ABD8A05027E"
-            DependsOn   = "[File]SQLBinaryDownload"
-        }
-        
         LocalConfigurationManager 
         { 
             CertificateId = $node.Thumbprint 
