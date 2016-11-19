6ef0cec49b77bcd2697b80c9265a50298c03d0a5
Davidniw <davidniw@hotmail.com>
2016-11-19 18:00:51 +1100

    Update EGBuildFarm.ps1

--- EGBuildFarm.ps1
+++ EGBuildFarm.ps1
@@ -52,7 +52,7 @@ configuration BuildFarm
             DestinationPath = "c:\software\Jetbrains\TeamCity"
             Credential = $storageCredential
             Ensure = "Present"
-            SourcePath = "\\prodevgcoresoftware.file.core.windows.net\software\Software\TeamCity\TeamCity-10.0.2.exe"
+            SourcePath = "\\prodevgcoresoftwareape.file.core.windows.net\software\Software\TeamCity\TeamCity-10.0.2.exe"
             Type = "File"
             Recurse = $false
         }
@@ -62,7 +62,7 @@ configuration BuildFarm
             DestinationPath = "c:\software\Microsoft\sqljdbc"
             Credential = $storageCredential
             Ensure = "Present"
-            SourcePath = "\\prodevgcoresoftware.file.core.windows.net\software\Software\TeamCity\sqljdbc_4.2.6420.100_enu.exe.lnk"
+            SourcePath = "\\prodevgcoresoftwareape.file.core.windows.net\software\Software\TeamCity\sqljdbc_4.2.6420.100_enu.exe.lnk"
             Type = "File"
             Recurse = $false
         }
@@ -72,7 +72,7 @@ configuration BuildFarm
             DestinationPath = "c:\software\Microsoft\BuildTools"
             Credential = $storageCredential
             Ensure = "Present"
-            SourcePath = "\\prodevgcoresoftware.file.core.windows.net\software\Software\TeamCity\BuildTools_2013_Full.exe"
+            SourcePath = "\\prodevgcoresoftwareape.file.core.windows.net\software\Software\TeamCity\BuildTools_2013_Full.exe"
             Type = "File"
             Recurse = $false
         }
@@ -82,11 +82,41 @@ configuration BuildFarm
             DestinationPath = "c:\software\Microsoft\BuildTools"
             Credential = $storageCredential
             Ensure = "Present"
-            SourcePath = "\\prodevgcoresoftware.file.core.windows.net\software\Software\TeamCity\BuildTools_2015_Full.exe"
+            SourcePath = "\\prodevgcoresoftwareape.file.core.windows.net\software\Software\TeamCity\BuildTools_2015_Full.exe"
             Type = "File"
             Recurse = $false
         }
-
+        
+        File BuildTools2015
+        {
+            DestinationPath = "c:\software\Jetbrains\TeamCity\Plugins"
+            Credential = $storageCredential
+            Ensure = "Present"
+            SourcePath = "\\prodevgcoresoftwareape.file.core.windows.net\software\Software\TeamCity\Plugins"
+            Type = "Directory"
+            Recurse = $false
+        }
+        
+        File BuildTools2015
+        {
+            DestinationPath = "c:\software\Jetbrains\TeamCity\Config"
+            Credential = $storageCredential
+            Ensure = "Present"
+            SourcePath = "\\prodevgcoresoftwareape.file.core.windows.net\software\Software\TeamCity\Config"
+            Type = "Directory"
+            Recurse = $false
+        }
+        
+        File BuildTools2015
+        {
+            DestinationPath = "c:\software\Jetbrains\TeamCity\stuff"
+            Credential = $storageCredential
+            Ensure = "Present"
+            SourcePath = "\\prodevgcoresoftwareape.file.core.windows.net\software\Software\TeamCity\stuff"
+            Type = "Directory"
+            Recurse = $false
+        }
+        
         WindowsFeature IIS
         {
             Ensure               = 'Absent'
