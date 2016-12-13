9f334fde66088ab8773bb3702104ff8181d40fe6
Davidniw <davidniw@hotmail.com>
2016-12-13 12:52:17 +1100

    Update BuildFarm.ps1

--- BuildFarm.ps1
+++ BuildFarm.ps1
@@ -434,106 +434,106 @@ configuration BuildFarm
     Node PrimaryDomainController
     {
 
-		WindowsFeature DNS_RSAT
-		{ 
-			Ensure = "Present" 
-			Name = "RSAT-DNS-Server"
-		}
+	WindowsFeature DNS_RSAT
+	{ 
+		Ensure = "Present" 
+		Name = "RSAT-DNS-Server"
+	}
 
-		WindowsFeature ADDS_Install 
-		{ 
-			Ensure = 'Present' 
-			Name = 'AD-Domain-Services' 
-		} 
+	WindowsFeature ADDS_Install 
+	{ 
+		Ensure = 'Present' 
+		Name = 'AD-Domain-Services' 
+	} 
 
-		WindowsFeature RSAT_AD_AdminCenter 
-		{
-			Ensure = 'Present'
-			Name   = 'RSAT-AD-AdminCenter'
-		}
+	WindowsFeature RSAT_AD_AdminCenter 
+	{
+		Ensure = 'Present'
+		Name   = 'RSAT-AD-AdminCenter'
+	}
 
-		WindowsFeature RSAT_ADDS 
-		{
-			Ensure = 'Present'
-			Name   = 'RSAT-ADDS'
-		}
+	WindowsFeature RSAT_ADDS 
+	{
+		Ensure = 'Present'
+		Name   = 'RSAT-ADDS'
+	}
 
-		WindowsFeature RSAT_AD_PowerShell 
-		{
-			Ensure = 'Present'
-			Name   = 'RSAT-AD-PowerShell'
-		}
+	WindowsFeature RSAT_AD_PowerShell 
+	{
+		Ensure = 'Present'
+		Name   = 'RSAT-AD-PowerShell'
+	}
 
-		WindowsFeature RSAT_AD_Tools 
-		{
-			Ensure = 'Present'
-			Name   = 'RSAT-AD-Tools'
-		}
+	WindowsFeature RSAT_AD_Tools 
+	{
+		Ensure = 'Present'
+		Name   = 'RSAT-AD-Tools'
+	}
 
-		WindowsFeature RSAT_Role_Tools 
-		{
-			Ensure = 'Present'
-			Name   = 'RSAT-Role-Tools'
-		}      
+	WindowsFeature RSAT_Role_Tools 
+	{
+		Ensure = 'Present'
+		Name   = 'RSAT-Role-Tools'
+	}      
 
-		WindowsFeature RSAT_GPMC 
-		{
-			Ensure = 'Present'
-			Name   = 'GPMC'
-		} 
+	WindowsFeature RSAT_GPMC 
+	{
+		Ensure = 'Present'
+		Name   = 'GPMC'
+	} 
 		
     }
 
 	Node BackupDomainController
     {
 
-		WindowsFeature DNS_RSAT
-		{ 
-			Ensure = "Present" 
-			Name = "RSAT-DNS-Server"
-		}
+	WindowsFeature DNS_RSAT
+	{ 
+		Ensure = "Present" 
+		Name = "RSAT-DNS-Server"
+	}
 
-		WindowsFeature ADDS_Install 
-		{ 
-			Ensure = 'Present' 
-			Name = 'AD-Domain-Services' 
-		} 
+	WindowsFeature ADDS_Install 
+	{ 
+		Ensure = 'Present' 
+		Name = 'AD-Domain-Services' 
+	} 
 
-		WindowsFeature RSAT_AD_AdminCenter 
-		{
-			Ensure = 'Present'
-			Name   = 'RSAT-AD-AdminCenter'
-		}
+	WindowsFeature RSAT_AD_AdminCenter 
+	{
+		Ensure = 'Present'
+		Name   = 'RSAT-AD-AdminCenter'
+	}
 
-		WindowsFeature RSAT_ADDS 
-		{
-			Ensure = 'Present'
-			Name   = 'RSAT-ADDS'
-		}
+	WindowsFeature RSAT_ADDS 
+	{
+		Ensure = 'Present'
+		Name   = 'RSAT-ADDS'
+	}
 
-		WindowsFeature RSAT_AD_PowerShell 
-		{
-			Ensure = 'Present'
-			Name   = 'RSAT-AD-PowerShell'
-		}
+	WindowsFeature RSAT_AD_PowerShell 
+	{
+		Ensure = 'Present'
+		Name   = 'RSAT-AD-PowerShell'
+	}
 
-		WindowsFeature RSAT_AD_Tools 
-		{
-			Ensure = 'Present'
-			Name   = 'RSAT-AD-Tools'
-		}
+	WindowsFeature RSAT_AD_Tools 
+	{
+		Ensure = 'Present'
+		Name   = 'RSAT-AD-Tools'
+	}
 
-		WindowsFeature RSAT_Role_Tools 
-		{
-			Ensure = 'Present'
-			Name   = 'RSAT-Role-Tools'
-		}      
+	WindowsFeature RSAT_Role_Tools 
+	{
+		Ensure = 'Present'
+		Name   = 'RSAT-Role-Tools'
+	}      
 
-		WindowsFeature RSAT_GPMC 
-		{
-			Ensure = 'Present'
-			Name   = 'GPMC'
-		} 
+	WindowsFeature RSAT_GPMC 
+	{
+		Ensure = 'Present'
+		Name   = 'GPMC'
+	} 
 		
     }
 
